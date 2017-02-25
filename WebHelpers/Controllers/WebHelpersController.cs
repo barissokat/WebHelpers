aef92a5428cdddce100d638fe88e9439c50f650e
bsokat <barissokat@gmail.com>
2017-02-25 18:39:23 +0300

    Create chart with single data source

---
+++ WebHelpers/Controllers/WebHelpersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebHelpers.Controllers
+{
+    public class WebHelpersController : Controller
+    {
+        // GET: WebHelpers
+        public ActionResult ChartSingleData()
+        {
+            
+            return View();
+        }
+    }
+}
