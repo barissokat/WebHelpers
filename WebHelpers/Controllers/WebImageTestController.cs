dedb7479d4100fc668bbe62422de2d79be5fc0e5
bsokat <barissokat@gmail.com>
2017-02-26 10:34:23 +0300

    Images added and new Controller added for WebImage helpers

---
+++ WebHelpers/Controllers/WebImageTestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHelpers.Controllers
+{
+    public class WebImageTestController : Controller
+    {
+        private string ImagePath
+        {
+            get
+            {
+                return Server.MapPath("~/Content/img1.jpg");
+            }
+        }
+
+        // GET: WebImageTest
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
